4dfeeb7f5a2dedd47fb6ca6f1d81e766c209ed57
Cy <chunyonggg@gmail.com>
2022-10-15 01:41:15 +0800

    limit range of time picker

--- src/components/DateTimePicker/index.tsx
+++ src/components/DateTimePicker/index.tsx
@@ -5,6 +5,7 @@ import {
   IonLabel,
   IonModal,
 } from '@ionic/react';
+import moment from 'moment';
 import styles from './styles.module.scss';
 
 export interface DateTimePickerProps {
@@ -25,6 +26,12 @@ export default function DateTimePicker({
   function handleChange(e: DatetimeCustomEvent) {
     onChange(e.detail.value ? (e.detail.value as string) : '');
   }
+  const fourteen = moment().add(15, 'days').toISOString();
+  const today = new Date().toISOString();
+  const todayDate = today.split('T')[0];
+  const min =
+    type === 'date' ? new Date().toISOString() : `${todayDate}T08:00:00.000Z`;
+  const max = type === 'date' ? fourteen : `${todayDate}T22:00:00.000Z`;
 
   return (
     <div className={styles['input-container']}>
@@ -37,7 +44,10 @@ export default function DateTimePicker({
       </div>
       <IonModal keepContentsMounted={true}>
         <IonDatetime
+          max={max}
+          min={min}
           value={value}
+          minuteValues="0,15,30,45"
           className={styles['date-picker']}
           id={`datetime-${label}`}
           presentation={type}
