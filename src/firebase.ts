c38dbbb366d87b066632a80110860fb25604eafa
Cy <chunyonggg@gmail.com>
2022-10-16 16:05:53 +0800

    change region

--- src/firebase.ts
+++ src/firebase.ts
@@ -23,4 +23,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 
-export const firestoreFunctions = getFunctions(app);
+export const firestoreFunctions = getFunctions(app, 'asia-southeast2');
