15226aee4f82ba26f8ca2ec4cb5f1eb04e619cc2
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2022-10-25 12:37:48 +0800

    Remove temp firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,15 +5,13 @@ importScripts(
 
 // Initialize the Firebase app in the service worker by passing the generated config
 const firebaseConfig = {
-  apiKey: 'AIzaSyBO6Gi5G3sPLolBRLIwLx8kL1TstRkzfk4',
-  authDomain: 'cs3216-final-group-9.firebaseapp.com',
-  databaseURL:
-    'https://cs3216-final-group-9-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'cs3216-final-group-9',
-  storageBucket: 'cs3216-final-group-9.appspot.com',
-  messagingSenderId: '212960746694',
-  appId: '1:212960746694:web:70195a9f1a203e62b48bc7',
-  measurementId: 'G-JRFQTGTPR5',
+  apiKey: 'AIzaSyDG5mnDMLiwKlP1c8aqJG-aSjRmxQ4j5LU',
+  authDomain: 'buddynus-production.firebaseapp.com',
+  projectId: 'buddynus-production',
+  storageBucket: 'buddynus-production.appspot.com',
+  messagingSenderId: '41559472129',
+  appId: '1:41559472129:web:1912a28e1eea15493ee800',
+  measurementId: 'G-KCCJ371DZ3',
 };
 
 firebase.initializeApp(firebaseConfig);
